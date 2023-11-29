81fcdc277b0283b20f9b6a2765964249eb3ac06a
jaeho1900 <glad1900@naver.com>
2023-11-30 08:31:02 +0900

    2311300830

--- financepy.py
+++ financepy.py
@@ -3,23 +3,39 @@
 import pandas_datareader, import pandas_datareader.data as pdr --> import yfinance as yf
 pdr.get_data_yahoo('종목코드') --> yf.download('종목코드')  # 야후종목
 """
+from urllib import parse
+from ast import literal_eval
+import requests
 
+def get_sise(code, start_time, end_time, time_from='day') :
+    get_param = {
+    	'symbol':code,
+	    'requestType':1,
+	    'startTime':start_time,
+	    'endTime':end_time,
+	    'timeframe':time_from
+    }
+    get_param = parse.urlencode(get_param)
+    url="https://api.finance.naver.com/siseJson.naver?%s"%(get_param)
+    response = requests.get(url, verify=False)
+    return literal_eval(response.text.strip())
+    
+get_sise('005930', '20210601', '20210605', 'day')
+
+
+# =======================
 import yfinance as yf
-yf.download('005930.KS')
-yf.download("AAPL")
+
+samsung = yf.download('005930.KS')
+close = samsung['Close']
+close['2019']
+close['2019-08']
+close['2019':'2020']
+close['2019':'2020'].plot(figsize=(15,7), color='red').grid()
+
+import plotly.express as px
+vol = samsung.Volume
+px.line(x=vol.index, y=vol)
 
 
 
-from datetime import datetime as dt
-import pytz
-import streamlit as st
-import yfinance as yf
-
-tz = pytz.timezone("America/New_York")
-start = tz.localize(dt(2013,1,1))
-end = tz.localize(dt.today())
-
-tickers = "MA,V,AMZN,JPM,BA".split(",")
-df = yf.download(tickers,start, end, auto_adjust=True)['Close']
-
-st.table(df.head())
