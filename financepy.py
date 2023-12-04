b38263cff43eb66e5eccea9430112586257d5d02
jaeho1900 <glad1900@naver.com>
2023-12-05 08:32:40 +0900

    202312050832

--- financepy.py
+++ financepy.py
@@ -79,6 +79,28 @@ data_close = go.Scatter(x=subset.index, y=subset['종가'], name='Close')
 fig = make_subplots(specs=[[{"secondary_y":True}]])
 fig.add_trace(data_vol, secondary_y=False)
 fig.add_trace(data_close, secondary_y=True)
+fig.update_layout(template='plotly_dark')
+fig.update_layout(title='Samsung Value')
+fig.update_layout(width=1100, height=500)
+fig.update_yaxes(title_text='거래량', secondary_y=False)
+fig.update_yaxes(title_text='주가', secondary_y=True)
 fig.show()
 
+# ===================
+import yfinance as yf
+
+div = yf.Ticker('0').dividends  # 0 은 '리얼티인컴'이라는 회사임
+div
+div.plot(figsize=(12,6))  # 주식분할 미반영
+
+div['2018':].plot(kind='bar', figsize=(15,7))
+
+
+
+
+
+
+# 타임라인 인덱스 형태 변경
+
+data.index = data.index.strftime('%Y-%m-%d')
 
