6a389efdcf2537f0ca00a2f009e8a0adb02c7e17
jaeho1900 <glad1900@naver.com>
2024-05-08 17:28:44 +0900

    240508

--- Test.py
+++ Test.py
@@ -1,2 +1,22 @@
-8 // 2 * (2 + 2)
+a = [1,2,3,4,5]
 
+j = 0
+for i in range(2, 0, -1):
+    i = i * (-1)
+    print(i)
+    a.insert(j, a.pop(i))
+    print(a)
+    j += 1
+
+
+
+
+    b.pop(i)
+    b[0] = c
+
+print(a,b)
+
+
+b = a.copy()
+a.pop(-2)
+a[0]= 9
