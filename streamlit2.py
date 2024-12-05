354832f60a44f7e645f0c76800b8cc2270edce43
jaeho1900 <glad1900@naver.com>
2024-12-06 08:39:27 +0900

    2412060839

--- streamlit2.py
+++ streamlit2.py
@@ -28,3 +28,35 @@ select_multi_species = st.sidebar.multiselect(
 )
 tmp_df = df[df['species'].isin(select_multi_species)]
 st.table(tmp_df)
+
+# 라디오에 선택한 내용을 radio select변수에 담습니다.
+radio_select =st.sidebar.radio(
+    "what is key column?",
+    ['sepal length', 'sepal width', 'petal length','petal width'],
+    horizontal=True
+    )
+
+# 선택한 컬럼의 값의 범위를 지정할 수 있는 slider를 만듭니다.
+slider_range = st.sidebar.slider(
+    "choose range of key column",
+     0.0,      #시작 값 
+     10.0,     #끝 값  
+    (2.5, 7.5) # 기본값, 앞 뒤로 2개 설정 /  하나만 하는 경우 value=2.5 이런 식으로 설정 가능
+)
+
+# 필터 적용버튼 생성
+start_button = st.sidebar.button(
+    "filter apply 📊" #"버튼에 표시될 내용"
+)
+
+# button 이 눌리는 경우 start_button의 값이 true로 바뀌게 된다.
+# 이를 이용해서 if문으로 버튼이 눌렸을 때를 구현 
+if start_button:
+    tmp_df = df[df['species'].isin(select_multi_species)]
+    #slider input으로 받은 값에 해당하는 값을 기준으로 데이터를 필터링합니다.
+    tmp_df= tmp_df[ (tmp_df[radio_select] >= slider_range[0]) & (tmp_df[radio_select] <= slider_range[1])]
+    st.table(tmp_df)
+    # 성공문구 + 풍선이 날리는 특수효과 
+    st.sidebar.success("Filter Applied!")
+    st.balloons()
+
