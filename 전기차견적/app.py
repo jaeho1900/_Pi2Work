bf336dce5188f35a58d138e50b6bc7140e6c3ad9
jaeho1900 <glad1900@naver.com>
2024-12-19 16:57:27 +0900

    2412191657

--- 전기차견적/app.py
+++ 전기차견적/app.py
@@ -2,42 +2,27 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
-# st.set_page_config(layout="wide")
-
 st.markdown('<h1 style="text-align: center;">견 적 서</h1>', unsafe_allow_html=True)
-st.image('상호.png')
+# st.image('상호.png')
 
-df = pd.DataFrame(
-    { "구분":["1.전기차 진화 시스템 구축비", "2.원격 관제 시스템 구축비", "3.원격관제시스템 월운영서비스료"],
-      "약정개월/층위치":['','',''],
-      "주차면수":['','',''],
-      "단가":['','',''],
-      "금액":['','',''],
-      "비고":['','',''] 
-    }
-)
-
-st.dataframe(df, use_container_width=True, hide_index=True)
-st.table(df)
-
-
-tab1, tab2, tab3= st.sidebar.tabs(['Tab A', 'Tab B', 'Tab C'])
+tab1, tab2, tab3= st.sidebar.tabs(['Step 1 ▷▷', 'Step 2 ▷▷', 'Step 3'])
 
 with tab1:
-  step1 = st.sidebar.radio(label = '① 전기차 진화 시스템 약정 기간 선택', options = ["무약정", "36개월(3년) 약정", "60개월(5년) 약정"])
+  step1 = st.radio(label = '① 전기차 진화 시스템 약정 기간 선택', options = ["무약정", "36개월(3년) 약정", "60개월(5년) 약정"])
 
 with tab2:
-  step2 = st.sidebar.radio(label = '② 원격 관제 시스템 약정 기간 선택', options = ["미선택", "무(일시납) 약정", "24개월(2년) 약정", "36개월(3년) 약정", "60개월(5년) 약정"])
+  step2 = st.radio(label = '② 원격 관제 시스템 약정 기간 선택', options = ["미선택", "무(일시납) 약정", "24개월(2년) 약정", "36개월(3년) 약정", "60개월(5년) 약정"])
 
 with tab3:
-  st.sidebar.markdown('<h5 style="text-align: left;">③ 설치장소 및 필요수량(연속공간 기준) 입력</h5>', unsafe_allow_html=True)
+  st.markdown('<h5 style="text-align: left;">③ 설치장소 및 필요수량(연속공간 기준) 입력</h5>', unsafe_allow_html=True)
   df_order = pd.DataFrame(
                     {"지하 층위치":['B1F'],
-                      "연속된 주차면수":['1']
+                     "연속된 주차면수":['1']
                     }
   )
-  st.sidebar.data_editor(
-      df_order, hide_index=True, num_rows="dynamic", use_container_width=True,
+
+  st.data_editor(
+      df_order, hide_index=True, num_rows="dynamic", use_container_width=True, key="추출", # 키값을 부여하여 세션스테이트와 연결
       column_config={
                     "지하 층위치": st.column_config.SelectboxColumn(
                                         label="지하 층위치",
@@ -58,3 +43,9 @@ with tab3:
                     }
   )
 
+st.divider()
+st.write(step1)
+st.write(step2)
+st.divider()
+
+st.write(st.session_state["추출"])
