70975045a121f818efd716a5e98a26ed92218acc
jaeho1900 <glad1900@naver.com>
2025-03-04 13:54:30 +0900

    1

--- ai.py
+++ ai.py
@@ -16,6 +16,6 @@ for i in range(20, 2001):
     else:
         print(2)
 
-
+# https://data.busan.go.kr/bdip/analysis/addressCleaning.do
 # https://marketplace.visualstudio.com/items?itemName=Google.geminicodeassist
 # https://bookshelf-journey.tistory.com/entry/%EC%B4%88%EB%B3%B4%EC%9E%90%EB%A5%BC-%EC%9C%84%ED%95%9C-Gemini-Code-Assist-for-individuals-%EC%82%AC%EC%9A%A9-%EA%B0%80%EC%9D%B4%EB%93%9C
