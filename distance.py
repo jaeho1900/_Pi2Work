f6dfde1f2dcbab812924b6c98b14fcddcf3e02b2
jaeho1900 <glad1900@naver.com>
2025-03-10 08:08:26 +0900

    1

--- distance.py
+++ distance.py
@@ -5,10 +5,10 @@
 import pandas as pd
 from geopy.distance import geodesic
 
-df = pd.read_excel('서울시건축물대장표제부.xlsx')
+df = pd.read_excel('표제부.xlsx')
 
-# 베이스캠프 위치 정의
-base_camps = {
+# 서비스센터 위치
+service_centers = {
     '마포강북': (37.5483390, 126.953640),
     '광화문': (37.5692569, 126.971556),
     '강서인천': (37.4821346, 126.879683),
@@ -17,26 +17,26 @@ base_camps = {
 
 def calculate_distances(row):
     location = (row['위도'], row['경도'])
-    distances = {name: geodesic(location, coords).km for name, coords in base_camps.items()}
+    distances = {name: geodesic(location, coords).km for name, coords in service_centers.items()}
     
     # 직선거리 계산
     nearest_direct = min(distances, key=distances.get)
     direct_distance = distances[nearest_direct]
     
-    # 맨해튼 거리 계산
-    manhattan_distances = {name: abs(location[0]-coords[0]) + abs(location[1]-coords[1]) for name, coords in base_camps.items()}
+    # 맨해튼거리 계산
+    manhattan_distances = {name: abs(location[0]-coords[0]) + abs(location[1]-coords[1]) for name, coords in service_centers.items()}
     nearest_manhattan = min(manhattan_distances, key=manhattan_distances.get)
     manhattan_distance = manhattan_distances[nearest_manhattan] * 111  # 대략적인 km 변환 (1도 ≈ 111km)
     
     return pd.Series({
-        '가장 가까운 베이스캠프(직선)': nearest_direct,
+        '서비스센터(직선)': nearest_direct,
         '직선거리(km)': direct_distance,
-        '가장 가까운 베이스캠프(맨해튼)': nearest_manhattan,
+        '서비스센터(맨해튼)': nearest_manhattan,
         '맨해튼거리(km)': manhattan_distance
     })
 
 # 새로운 열 추가
-df[['베이스캠프(직선)', '직선거리(km)', '베이스캠프(맨해튼)', '맨해튼거리(km)']] = df.apply(calculate_distances, axis=1)
+df[['서비스센터(직선)', '직선거리(km)', '서비스센터(맨해튼)', '맨해튼거리(km)']] = df.apply(calculate_distances, axis=1)
 
 # 결과를 엑셀 파일로 저장
-df.to_excel('서울시건축물대장표제부_distance.xlsx', index=False)
+df.to_excel('표제부_distance.xlsx', index=False)
--- distribution_map.py
+++ distribution_map.py
@@ -3,7 +3,7 @@ import folium
 from folium.plugins import MarkerCluster
 
 # 엑셀 파일 읽기
-df = pd.read_excel('서울시건축물대장표제부.xlsx')
+df = pd.read_excel('표제부.xlsx')
 
 # 지도 생성 (첫 번째 좌표를 중심으로 설정)
 map_center = [df['위도'].iloc[0], df['경도'].iloc[0]]
@@ -14,15 +14,14 @@ marker_cluster = MarkerCluster().add_to(m)
 
 # 유형별 색상 정의
 color_dict = {
-    '판매시설': 'red',
-    '근생시설': 'blue',
-    '업무시설': 'black',
-    '의료시설': 'green'
+    '근생시설': 'green',
+    '업무시설': 'orange',
+    '판매시설': 'purple',
+    '의료시설': 'darkblue'
 }
 
 # 각 좌표에 마커 추가
 for index, row in df.iterrows():
-    # 유형에 따라 색상 선택 (기본값은 gray)
     color = color_dict.get(row['주용도'], 'gray')
     
     folium.Marker(
@@ -36,10 +35,10 @@ legend_html = '''
 <div style="position: fixed; bottom: 50px; left: 50px; width: 120px; height: 90px; 
     border:2px solid grey; z-index:9999; font-size:14px; background-color:white;
     ">&nbsp;<b>범례:</b><br>
-    &nbsp;<i class="fa fa-map-marker fa-2x" style="color:red"></i> 판매시설<br>
-    &nbsp;<i class="fa fa-map-marker fa-2x" style="color:blue"></i> 근생시설<br>
-    &nbsp;<i class="fa fa-map-marker fa-2x" style="color:black"></i> 업무시설<br>
-    &nbsp;<i class="fa fa-map-marker fa-2x" style="color:green"></i> 의료시설
+    &nbsp;<i class="fa fa-map-marker fa-2x" style="color:green"></i> 근생시설<br>
+    &nbsp;<i class="fa fa-map-marker fa-2x" style="color:orange"></i> 업무시설<br>
+    &nbsp;<i class="fa fa-map-marker fa-2x" style="color:purple"></i> 판매시설<br>
+    &nbsp;<i class="fa fa-map-marker fa-2x" style="color:darkblue"></i> 의료시설
 </div>
 '''
 m.get_root().html.add_child(folium.Element(legend_html))
