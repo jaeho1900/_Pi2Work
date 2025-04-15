6fac771a9a661f9e2852e359c2d01286ae15bee4
jaeho1900 <glad1900@naver.com>
2025-04-15 14:51:25 +0900

    2252

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
-import sqlalchemy
 import pandas as pd
 
-df = pd.read_csv('D:/mart_djy_03.txt', delimiter='|', nrows= 10, header=None)
+df = pd.read_csv('D:/mart_djy_03.txt', delimiter='|', nrows= 10000, header=None)
 print (df)
 
 df.to_csv("D:/save10.txt", sep='|', header=False, index=False)
 
-df2 = pd.read_csv('D:/save10.txt', delimiter='|', nrows= 10, header=None)
+df2 = pd.read_csv('D:/save10.txt', delimiter='|', header=None)
 print (df2)
 
